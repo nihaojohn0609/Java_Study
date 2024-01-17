8fde0b0c1f257d6e2d8e4f2f77dfc54e1edeb830
nihaojohn0609 <130543530+nihaojohn0609@users.noreply.github.com>
2024-01-17 22:55:17 +0900

    Add files via upload
    min

---
+++ GameConsole/src/dev/lpa/sealed/SpecialAbstractClass.java
@@ -0,0 +1,8 @@
+package dev.lpa.sealed;
+
+public sealed abstract class SpecialAbstractClass permits FinalKid, NonSealedKid, SealedKid, SpecialAbstractClass.Kid {
+
+    final class Kid extends SpecialAbstractClass {
+
+    }
+}
